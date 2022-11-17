b5448f9d94f0e69bfd08064cdfd74a888d6c3018
acuev <abcuevas@csumb.edu>
2022-11-16 18:12:26 -0800

    add webpages

--- Otterkitchen.py
+++ Otterkitchen.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template
+import requests, json
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+my_key = '601b6bca78294aaf9ea387a7157142bf'
 
+endpoint1 = 'https://api.spoonacular.com/recipes/random?apiKey=601b6bca78294aaf9ea387a7157142bf'
+
+
+@app.route("/random")
+def index():
+    try:
+        r = requests.get(endpoint1)
+        data = r.json()
+        print(data)
+    except:
+        print('please try again')
+    return render_template("random.html", data = data)
 
 @app.route("/")
-def index():
+def main():
     return render_template("frontpage.html")
+
+endpoint = 'https://api.spoonacular.com/recipes/random?apiKey=601b6bca78294aaf9ea387a7157142bf&number=3&tags=vegetarian&instructionsRequired=true'
+@app.route("/vegetarian")
+def veggie():
+    try:
+        r = requests.get(endpoint)
+        data = r.json()
+        print(data)
+    except:
+        print('please try again')
+    return render_template("vegetarian.html", data = data)
