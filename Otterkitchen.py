5b54b0247ed35a0f13fe5b3ff975f54df5da1ab4
Angel Bedolla <angel.alcantar233@gmail.com>
2022-12-04 17:54:55 -0800

    Fixed formatting

--- Otterkitchen.py
+++ Otterkitchen.py
@@ -6,9 +6,9 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-my_key = '601b6bca78294aaf9ea387a7157142bf'
+my_key = 'd53b43f7d9d14401a56568b6af67fe2e'
 
-endpoint1 = 'https://api.spoonacular.com/recipes/random?apiKey=601b6bca78294aaf9ea387a7157142bf'
+endpoint1 = f'https://api.spoonacular.com/recipes/random?apiKey={my_key}'
 
 
 @app.route("/random")
@@ -25,24 +25,35 @@ def index():
 def main():
     return render_template("frontpage.html")
 
-endpoint = 'https://api.spoonacular.com/recipes/random?apiKey=601b6bca78294aaf9ea387a7157142bf&number=3&tags=vegetarian&instructionsRequired=true'
+endpoint1 = f'https://api.spoonacular.com/recipes/random?apiKey={my_key}&number=3&tags=vegetarian&instructionsRequired=true'
 @app.route("/vegetarian")
 def veggie():
     try:
-        r = requests.get(endpoint)
+        r = requests.get(endpoint1)
         data = r.json()
-        print(data)
+        # print(data)
     except:
         print('please try again')
     return render_template("vegetarian.html", data = data)
 
-endpoint = 'https://api.spoonacular.com/recipes/random?apiKey=601b6bca78294aaf9ea387a7157142bf&number=3&type=breakfast&instructionsRequired=true'
+endpoint1 = f'https://api.spoonacular.com/recipes/random?apiKey={my_key}&number=3&type=breakfast&instructionsRequired=true'
 @app.route("/breakfast")
 def breakfast():
     try:
-        r = requests.get(endpoint)
+        r = requests.get(endpoint1)
         data = r.json()
-        print(data)
+        # print(data)
     except:
         print('please try again')
     return render_template("breakfast.html", data = data)
+
+endpoint = f'https://api.spoonacular.com/recipes/autocomplete?number=4&query=sandwhich&apiKey={my_key}&'
+@app.route("/sandwhich")
+def sandwhich():
+    try:
+        r = requests.get(endpoint)
+        data = r.json()
+        # print(data)
+    except:
+        print('please try again')
+    return render_template("sandwhich.html", data = data)
